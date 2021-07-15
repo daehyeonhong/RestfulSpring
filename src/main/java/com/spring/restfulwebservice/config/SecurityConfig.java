db24eca03d6d88ce3733dd7bdbf692a0f5d34aac
DaeHyeonHong <hongdae@zen9ss.co.kr>
2021-07-15 16:30:08 +0900

    security

---
+++ src/main/java/com/spring/restfulwebservice/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.spring.restfulwebservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("kenneth")
+                .password("{noop}test1234")
+                .roles("USER");
+    }
+
+}
